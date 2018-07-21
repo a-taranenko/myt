68e835bcb0c87b9b0963c80f1078241bd594a38c
Anton Taranenko <antontaranenko@Antons-MacBook-Air.local>
2018-07-21 16:27:22 -0600

    Created LogIn component

---
+++ src/LogIn.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class LogIn extends Comment {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <form>
+          Username: <input type="text" /><br />
+          Password: <input type="text" /><br />
+          <button>Log In</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LogIn
