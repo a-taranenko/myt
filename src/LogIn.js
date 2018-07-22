86cd036095af0c6c99bb2ee1d2d40fea974906c5
Anton Taranenko <antontaranenko@Antons-MacBook-Air.local>
2018-07-21 19:46:10 -0600

    Added labels in LogIn

--- src/LogIn.js
+++ src/LogIn.js
@@ -5,14 +5,16 @@ class LogIn extends Component {
     return (
       <div className="login">
         <form onSubmit={this.props.logger}>
-          Username: <input type="text"
-                           id="username"
-                           value={this.props.username}
-                           onChange={this.props.handleInput} /><br />
-          Password: <input type="password"
-                           id="password"
-                           value={this.props.password}
-                           onChange={this.props.handleInput} /><br />
+          <label htmlFor="username">Username</label><br />
+          <input type="text"
+                 id="username"
+                 value={this.props.username}
+                 onChange={this.props.handleInput} /><br />
+          <label htmlFor="password">Password</label><br />
+          <input type="password"
+                 id="password"
+                 value={this.props.password}
+                 onChange={this.props.handleInput} /><br />
           <button>Log In</button>
         </form>
       </div>
