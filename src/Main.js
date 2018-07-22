7ebee2536c85538f3385eda8c9be184b3025f68c
Anton Taranenko <antontaranenko@Antons-MacBook-Air.local>
2018-07-21 21:01:23 -0600

    Added state to Main

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      companyName: "",
+      superUser: false
+    }
+  }
+
+  componentWillMount() {
+    // The following is just temporary
+    // Need to call db to get the following info
+
+    this.setState({
+      companyName: "Default Company Name",
+      superUser: true
+    })
+  }
+
   render() {
     return (
       <div className="main">
+        <h1>{this.state.companyName}</h1>
         <p>Hello {this.props.username}</p>
         <button onClick={this.props.logout}>Log out</button>
       </div>
